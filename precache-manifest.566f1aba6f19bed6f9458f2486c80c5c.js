c0830f2bfdf957542b3d61ae71ec90a6e58df833
dayongbz <dayongbz@gmail.com>
2019-06-09 17:15:54 +0900

    Updates

---
+++ precache-manifest.566f1aba6f19bed6f9458f2486c80c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b1e074d8f031742eacee9dc5fb78fa",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d978566d5ce695a35b29",
+    "url": "/movie_app/static/css/main.04d8132d.chunk.css"
+  },
+  {
+    "revision": "82570cd7efab72a4b435",
+    "url": "/movie_app/static/js/2.e93332a3.chunk.js"
+  },
+  {
+    "revision": "d978566d5ce695a35b29",
+    "url": "/movie_app/static/js/main.34ef085b.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
